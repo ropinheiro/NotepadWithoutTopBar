cc84545894c6e469b0c665264bd43875cb1509d8
Rui Pinheiro <rpinheiro@neoception.com>
2020-09-21 00:14:37 +0100

    First dump of all the code

---
+++ NWTB.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace NotepadWithoutTopBar
+{
+    partial class frmNWTB
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtText
+            // 
+            this.txtText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtText.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtText.Location = new System.Drawing.Point(0, 0);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(1399, 68);
+            this.txtText.TabIndex = 0;
+            this.txtText.Text = "<Write here. Use SHIFT+Cursor to move slowly, CTRL+cursor to move in jumps>";
+            // 
+            // frmNWTB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1399, 68);
+            this.ControlBox = false;
+            this.Controls.Add(this.txtText);
+            this.Name = "frmNWTB";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtText;
+    }
+}
+
---
+++ NWTB.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Forms;
+
+namespace NotepadWithoutTopBar
+{
+    public partial class frmNWTB : Form
+    {
+        public frmNWTB()
+        {
+            InitializeComponent();
+        }
+
+        protected override bool ProcessCmdKey( ref Message msg, Keys keyData )
+        {
+            // Small steps
+            if ( keyData == ( Keys.Left | Keys.Shift ) )
+                Left--;
+
+            if ( keyData == ( Keys.Right | Keys.Shift ) )
+                Left++;
+
+            if ( keyData == ( Keys.Up | Keys.Shift ) )
+                Top--;
+
+            if ( keyData == ( Keys.Down | Keys.Shift ) )
+                Top++;
+
+            // Big steps
+            if ( keyData == ( Keys.Left | Keys.Control ) )
+                Left -= 10;
+
+            if ( keyData == ( Keys.Right | Keys.Control ) )
+                Left += 10;
+
+            if ( keyData == ( Keys.Up | Keys.Control ) )
+                Top -= 10;
+
+            if ( keyData == ( Keys.Down | Keys.Control ) )
+                Top += 10;
+
+            return base.ProcessCmdKey( ref msg, keyData );
+        }
+    }
+}
